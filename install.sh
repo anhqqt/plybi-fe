d0ff580619b91a8ba115effcef8926d06bac8a95
Anh Quach <tuananh.qaa@gmail.com>
2021-01-22 17:18:00 +0700

    Complete new version with using 2 docker-compose.yml file

--- install.sh
+++ install.sh
@@ -23,18 +23,16 @@ echo "- https://github.com/sheid1309"
 echo "-------------------------------------------------------------------------"
 echo "Please follow below guides:"
 echo "- Step 1: Install Docker to your machine"
-echo "- Step 2: Run Nginx container on port 80 with your code inside"
-echo "- Step 3: Run Certbot to generate SSL Certificates to your Nginx container"
-echo "- Step 4: Re-configure Nginx container with new SSL"
-echo "- Step 5: Update new source code by re-run all container"
+echo "- Step 2: Run Nginx container and generate SSL with Certbot"
+echo "- Step 3: Re-configure Nginx container with new SSL and Auto Renewal"
+echo "- Step 4: Update new source code by re-run all container"
 echo "-------------------------------------------------------------------------"
 echo
 options=(
     "Step 1: Install Docker" #1
-    "Step 2: Run Nginx container" #2
-    "Step 3: Run Certbot container" #3
-    "Step 4: Re-configure Nginx" #4
-    "Step 5: Rebuild new source code" #4
+    "Step 2: Generate SSL" #2
+    "Step 3: Run Nginx with SSL and Auto Renewal" #3
+    "Step 4: Update new source code" #4
 )
 
 prompt="Please enter your choice (1-${#options[@]})[Ctrl+C to exit]: "
@@ -51,8 +49,6 @@ do
             clear; step_3;;
         "${options[3]}")
             clear; step_4;;
-        "${options[4]}")
-            clear; step_5;;
         *) echo "Invalid input. Please try again with valid number from 1-${#options[@]}[Ctrl+C to exit]: "; continue;;
     esac
 done
